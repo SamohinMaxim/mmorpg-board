eb32e9339942ebba274a30dae9a5bf57294ad8b7
SamokhinMaxim <Samohin721@yandex.ru>
2026-05-11 01:45:45 +0500

    Remove password

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -156,7 +156,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = 'test.newsportal@yandex.ru'
-EMAIL_HOST_PASSWORD = 'btpgtcsovnsqxssj'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_SSL = True
 DEFAULT_FROM_EMAIL = 'test.newsportal@yandex.ru'
 
